01e381a00b307791470e770a7e139b06601d1e2f
chourdakisk <chourdakisk@gmail.com>
2015-10-28 19:39:18 +0000

    Special Offer Methods

--- src/shoppingbasket/BillCalculation.java
+++ src/shoppingbasket/BillCalculation.java
@@ -6,8 +6,22 @@ package shoppingbasket;
  */
 public class BillCalculation {
 
-    public void offerCalculation(BasketItem basketItem) {
-        int newQuantity = basketItem.getQuantity();
+    int newQuantity = 0;
+
+    public void BuyOneGetOneFreeOfferCalculation(BasketItem basketItem) {
+        newQuantity = basketItem.getQuantity();
+
+        if (basketItem.getQuantity() > 1) {
+            newQuantity = basketItem.getQuantity() - (basketItem.getQuantity() / 2);
+        }
+    }
+
+    public void ThreeForThePriceOfTwoOfferCalculation(BasketItem basketItem) {
+        newQuantity = basketItem.getQuantity();
+
+        if (basketItem.getQuantity() > 2) {
+            newQuantity = basketItem.getQuantity() - (basketItem.getQuantity() / 3);
+        }
     }
 
 }
